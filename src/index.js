1b6e32f132e03712b9dd65acf653b4acd2bc3dd5
T3rex <31337aditya@gmail.com>
2025-07-01 13:51:45 +0530

    Added location API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+const data = require("./data.json");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+let index = 0;
+
+app.get("/api/location", (req, res) => {
+  if (index >= 0 && index < data.length) {
+    return res.status(200).json(data[index++]);
+  } else {
+    if (index >= data.length) {
+      index = 0;
+      return res.status(204).send();
+    }
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
